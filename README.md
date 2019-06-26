76330dd714cbf238bfc899dfc809e1080acb4e24
Meghan Mullally <ms.mullally27@gmail.com>
2019-06-26 07:01:44 -0700

    added variables, fixed readme built me section

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 ### Built with 
 
-[axios](https://www.npmjs.com/package/axios)
-[spotify api](https://www.npmjs.com/package/node-spotify-api)
-[moment](https://www.npmjs.com/package/moment)
-[dotENV](https://www.npmjs.com/package/dotenv)
+- [axios](https://www.npmjs.com/package/axios)
+- [spotify api](https://www.npmjs.com/package/node-spotify-api)
+- [moment](https://www.npmjs.com/package/moment)
+- [dotENV](https://www.npmjs.com/package/dotenv)
--- liri.js
+++ liri.js
@@ -1,3 +1,37 @@
 require("dotenv").config();
 
 
+var request = require("request");
+
+// grabs keys 
+var keys = require("./keys.js");
+
+//command and input 
+var command = process.argv[2];
+var input = process.argv[3];
+
+
+
+// Spotify &  moment 
+var Spotify = require('node-spotify-api')
+var spotify = new Spotify(keys.spotify);
+// moment 
+var moment = require('moment')
+
+var fs = require('fs')
+
+// Spotify song commands 
+
+function spotifySong(song) {
+  if (song === null) {
+    song = ''
+  }
+}
+
+// concert-this
+
+// spotify-this-song
+
+// movie-this
+
+// do-what-it-says
