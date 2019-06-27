29e09a01a4a8e5a7110ffb0d0bd8cb81637c8ee5
Meghan Mullally <ms.mullally27@gmail.com>
2019-06-26 19:50:12 -0700

    created a getBand function and created a switch case to execute 1 of many codes blocks

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 
 ### Built with 
 
+
 - [axios](https://www.npmjs.com/package/axios)
 - [spotify api](https://www.npmjs.com/package/node-spotify-api)
 - [moment](https://www.npmjs.com/package/moment)
--- liri.js
+++ liri.js
@@ -1,37 +1,87 @@
 require("dotenv").config();
 
-
-var request = require("request");
-
-// grabs keys 
+// imports code from keys.js and stores it in a variable 
 var keys = require("./keys.js");
+// Spotify &  moment 
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+var moment = require('moment')
+var fs = require('fs')
+
+//calling axios 
+var axios = require("axios");
+
 
 //command and input 
 var command = process.argv[2];
 var input = process.argv[3];
 
+//default movie & song 
+var defaultMovie = "Mr. Nobody";
+var defaultSong = "The Sign";
 
 
-// Spotify &  moment 
-var Spotify = require('node-spotify-api')
-var spotify = new Spotify(keys.spotify);
-// moment 
-var moment = require('moment')
+//Switch Case to select one of many code blocks to be executed 
 
-var fs = require('fs')
+switch (command) {
+  case "concert-this":
+    getBands(value)
 
-// Spotify song commands 
+    break;
+  case "spotify-this-song":
+    if (value === "") {
+      value = defaultSong
+    }
+    getSongs(value)
+    break;
 
-function spotifySong(song) {
-  if (song === null) {
-    song = ''
-  }
+  case "movie-this":
+    //if user input is not added, default to defaultMovie
+    if (value === "") {
+      value = defaultMovie
+    }
+    getMovies(value)
+    break;
+  case "do-what-it-says":
+    doWhatItSays()
+    break;
+
+  default:
+    break;
 }
 
-// concert-this
+// CONCERT-THIS : getBands function 
+
+function getBands(artist) {
+  //axios
+  // using get to grab from the URL
+  axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+    .then(function (response) {
+      console.log("Name of the Venue: " + response.data[0].venue.name)
+      console.log("Location: " + response.data[0].venue.city)
+      
+      //using MOMENT to format the eventDate 
+      var eventDate = moment(response.data[0].datatime).format('MM/DD/YYYY')
+      
+      console.log("Date of Event: " + eventDate)
+      
+      
+      
+      // console.log(response);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+
+}
+
+
+
 
 // spotify-this-song
 
 // movie-this
+
+//http://www.imdb.com/title/tt0485947/
 
 // do-what-it-says
